da91c79b3f362cbba2697beb7045cb1fb103d668
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-04-15 18:11:49 -0700

    Add TypeScript support

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Page() {
+  return (
+    <View>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </View>
+  );
+}
