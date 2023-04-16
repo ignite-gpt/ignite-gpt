9de5f50dd951a9bc676a7c06de6ac4d73758aca4
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-04-15 19:34:25 -0700

    Add Input and Button

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,33 @@
-import { Button, Center } from 'native-base'
+import { Button, Center, Column, Flex, Input, Text } from 'native-base'
+import { useState } from 'react'
 
 export default function Page() {
+  const [tempo, setTempo] = useState(120)
+  const [tempoInput, setTempoInput] = useState(tempo.toString())
+
   return (
     <Center height="100%" width="100%">
-      <Button onPress={() => console.log('Hello world')}>Hello world</Button>
+      <Column width="50%" space={4}>
+        <Flex direction="row" justifyContent="center">
+          <Text fontSize="5xl">{tempo}</Text>
+        </Flex>
+        <Input
+          placeholder="Tempo"
+          value={tempoInput}
+          onChangeText={setTempoInput}
+        />
+        <Button
+          onPress={() =>
+            setTempo(
+              Number.isNaN(parseInt(tempoInput, 10))
+                ? tempo
+                : parseInt(tempoInput, 10)
+            )
+          }
+        >
+          Update
+        </Button>
+      </Column>
     </Center>
   )
 }
