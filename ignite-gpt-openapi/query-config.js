8d5085c9f4bfcd338be3fedba76fa7f34f1a9e1a
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-05-19 08:13:12 -0700

    Set up RTK Query Codegen

---
+++ ignite-gpt-openapi/query-config.js
@@ -0,0 +1,8 @@
+const config = {
+  schemaFile: './openapi.json',
+  apiFile: '../ignite-gpt-app/store/emptyApi.ts',
+  outputFile: '../ignite-gpt-app/store/igniteApi.ts',
+  hooks: true,
+};
+
+module.exports.default = config;
