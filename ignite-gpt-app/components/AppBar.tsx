5a0b69db714724f2f1ac0680c86db8712133688c
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-05-23 11:42:37 -0700

    Add badge to app bar

--- ignite-gpt-app/components/AppBar.tsx
+++ ignite-gpt-app/components/AppBar.tsx
@@ -1,5 +1,20 @@
 import { Stack } from 'expo-router'
+import { Badge, Heading, Row } from 'native-base'
 
 export default function AppBar({ title = 'IgniteGPT' }) {
-  return <Stack.Screen options={{ title }} />
+  return (
+    <Stack.Screen
+      options={{
+        title,
+        headerTitle: ({ children }) => (
+          <Row space={4}>
+            <Heading size="lg" color="white">
+              {children}
+            </Heading>
+            <Badge alignSelf="center">pre-alpha</Badge>
+          </Row>
+        ),
+      }}
+    />
+  )
 }
